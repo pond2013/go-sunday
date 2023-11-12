e08aa8b92830cb8c8c1abe44ee50051719ce41b5
Pawaraid Niyomlao <pondhub@odds.team>
2023-11-12 03:56:37 +0000

    example conditional

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"strconv"
 )
 
 func main() {
-	//example input string
+	// Example input string
 	/*
 		fmt.Println("enter your name:")
 
@@ -19,27 +17,59 @@ func main() {
 	*/
 
 	// check variable type
-	age := 2
-	fmt.Println(reflect.TypeOf(age))
+	/*
+		age := 2
+		fmt.Println(reflect.TypeOf(age))
+	*/
 
 	// convert type
-	height := "100"
-	h, err := strconv.Atoi(height)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(h)
+	/*
+		height := "100"
+		h, err := strconv.Atoi(height)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(h)
+		}
+
+		water := "12.40"
+		fWater, err := strconv.ParseFloat(water, 32)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(fWater)
+		}
+
+		txtShow := ""
+		txtShow = fmt.Sprintf("water = %.2f, height = %d", fWater, h)
+		fmt.Println(txtShow)
+	*/
+
+	// Iteration for
+	/*
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	*/
+
+	// Switch case
+	score := 80
+	switch score {
+	case 80:
+		{
+			fmt.Println("A")
+		}
+	case 70:
+		{
+			fmt.Println("B")
+		}
 	}
 
-	water := "12.40"
-	fWater, err := strconv.ParseFloat(water, 32)
-	if err != nil {
-		fmt.Println(err)
+	if score >= 80 {
+		fmt.Println("A")
+	} else if score < 80 && score > 70 {
+		fmt.Println("B")
 	} else {
-		fmt.Println(fWater)
+		fmt.Println("unknown")
 	}
-
-	txtShow := ""
-	txtShow = fmt.Sprintf("water = %.2f, height = %d", fWater, h)
-	fmt.Println(txtShow)
 }
