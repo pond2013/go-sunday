84cb0a011b17014a9b5085cf83fb394da668b2e6
Pawaraid Niyomlao <pondhub@odds.team>
2023-11-12 03:43:50 +0000

    example input value, convert value

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("testtest")
+	//example input string
+	/*
+		fmt.Println("enter your name:")
+
+		// var txtInput string
+		txtInput := ""
+
+		fmt.Scan(&txtInput)
+		fmt.Println("Hello " + txtInput)
+	*/
+
+	// check variable type
+	age := 2
+	fmt.Println(reflect.TypeOf(age))
+
+	// convert type
+	height := "100"
+	h, err := strconv.Atoi(height)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(h)
+	}
+
+	water := "12.40"
+	fWater, err := strconv.ParseFloat(water, 32)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(fWater)
+	}
 }
