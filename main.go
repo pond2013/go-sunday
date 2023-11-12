6926f2d91996d9dcd256f8308b6ac6a82292a3c7
Pawaraid Niyomlao <pondhub@odds.team>
2023-11-12 07:14:34 +0000

    example interface

--- main.go
+++ main.go
@@ -1,80 +1,34 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-)
+import "fmt"
 
-type Address struct {
-	Address  string
-	PostCode string
+type SaveLogInterface interface {
+	SaveLog()
 }
 
-type UserProfile struct {
-	Firstname string `json:"first_name"`
-	Lastname  string `json:"LastName"`
-	Age       int
-	Height    float32
-
-	Address Address
-
-	Bill struct {
-		BillAddress string
-	}
+func SaveLog(st SaveLogInterface) {
+	st.SaveLog()
 }
 
-func (u UserProfile) ToFullDesc() string {
-	return fmt.Sprintf("%s %s", u.Firstname, u.Lastname)
+type TransferBBL struct {
+	name string
+}
+
+type TransferKTB struct {
+	name string
+}
+
+func (tBBL TransferBBL) SaveLog() {
+	fmt.Println("save to database", tBBL)
+}
+
+func (tKTB TransferKTB) SaveLog() {
+	fmt.Println("save to database", tKTB)
 }
 
 func main() {
-	fmt.Println("xx")
-
-	/* example map */
-	user := map[string]string{}
-	user["username"] = "pondhub"
-	user["password"] = "xxxxxx"
-	fmt.Println(user)
-	fmt.Println(user["username"])
-
-	/* example struct */
-	userProfile := UserProfile{
-		Firstname: "pondhub",
-		Lastname:  "salty",
-		Age:       24,
-		Height:    160,
-	}
-	fmt.Println(userProfile)
-	userProfile.Address.PostCode = "11203"
-	fmt.Println(userProfile.ToFullDesc())
-	byteTxtJson, err := json.MarshalIndent(userProfile, "", "  ")
-	if err != nil {
-		fmt.Println("err")
-	} else {
-		fmt.Println(string(byteTxtJson))
-	}
-
-	/* Json data define */
-	dataJson :=
-		`{
-		"first_name": "sad",
-		"LastName": "salty",
-		"Age": 24,
-		"Height": 160,
-		"Address": {
-		  "Address": "",
-		  "PostCode": "11203"
-		},
-		"Bill": {
-		  "BillAddress": ""
-		}
-	}`
-	var myProfile UserProfile
-	err = json.Unmarshal([]byte(dataJson), &myProfile)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println((myProfile))
-	}
+	transA := TransferBBL{name: "BBL"}
+	transB := TransferKTB{name: "KTB"}
+	SaveLog(&transA)
+	SaveLog(&transB)
 }
