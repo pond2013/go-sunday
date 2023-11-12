7133c6ac513a01a5ae57ecc917132166ed62c81c
Pawaraid Niyomlao <pondhub@odds.team>
2023-11-12 07:16:35 +0000

    update pointer interface

--- main.go
+++ main.go
@@ -18,17 +18,17 @@ type TransferKTB struct {
 	name string
 }
 
-func (tBBL TransferBBL) SaveLog() {
-	fmt.Println("save to database", tBBL)
+func (tBBL *TransferBBL) SaveLog() {
+	fmt.Println("save to database", tBBL.name)
 }
 
-func (tKTB TransferKTB) SaveLog() {
-	fmt.Println("save to database", tKTB)
+func (tKTB *TransferKTB) SaveLog() {
+	fmt.Println("save to database", tKTB.name)
 }
 
 func main() {
-	transA := TransferBBL{name: "BBL"}
-	transB := TransferKTB{name: "KTB"}
+	transA := TransferBBL{name: "BBL test"}
+	transB := TransferKTB{name: "KTB test"}
 	SaveLog(&transA)
 	SaveLog(&transB)
 }
